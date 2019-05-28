1bea69e3637364f93bb8b7749fa2eeaf4f399a88
hyunjun <hunjun7209@naver.com>
2019-05-28 22:05:37 +0900

    정수 계산기

--- hw.c/hw.c/pch.cpp
+++ hw.c/hw.c/pch.cpp
@@ -4,37 +4,64 @@
 
 // 일반적으로 이 파일을 무시하지만 미리 컴파일된 헤더를 사용하는 경우 유지합니다.
 #include<stdio.h>
-#include<string.h>
-
-int check(char s[], char a[], char ch);
+int plus(int x, int y);
+int minus(int x, int y);
+int multiply(int x, int y);
+int division(int x, int y);
+int remainder(int x, int y);
+void print_menu()
+{
+	printf("1. +\n2. -\n3. *\n4. /\n5. %%\n");
+}
 int main()
 {
-	char solution[100] = "meet at midnight"; //정답이 들어있는 문자 배열
-	char answer[100] = "____ __ ________"; //현재까지 사용자가 맞춘 문자열이 들어 있는 문자 배열
-	char ch;
-
-	while (1)
-	{
-		printf("문자열을 입력하시오: %s \n", answer); //문자열 선택
-		printf("글자를 추측하시오: "); //선택한 문자열에 들어갈 글자 추측
-		ch = getchar();
-		if (check(solution, answer, ch) == 1)
-			break;
-		getchar(); //줄바꿈 문자 제거
-	}
-	return 0;
+	int x, y, sol;
+	print_menu();
+	printf("메뉴를 고르시오: ");
+	scanf_s("%d", &sol);
+	printf("\n수를 입력하십시오.\n");
+	printf("x= ");
+	scanf_s("%d", &x);
+	printf("y= ");
+	scanf_s("%d", &y);
+	if (sol == 1)
+		printf("%d + %d = %d", x, y, plus(x, y));
+	else if (sol == 2)
+		printf("%d - %d = %d", x, y, minus(x, y));
+	else if (sol == 3)
+		printf("%d * %d = %d", x, y, multiply(x, y));
+	else if (sol == 4)
+		printf("%d / %d = %d", x, y, division(x, y));
+	else if (sol == 5)
+		printf("%d %% %d = %d", x, y, remainder(x, y));
 }
-int check(char s[], char a[], char ch) //사용자로부터 답을 받아 정답과 비교하여 맞는지 틀린지 구분하는 함수
+int plus(int x, int y)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++) //정답 문자열의 끝까지 반복
-	{
-		if (s[i] == ch)
-			a[i] = ch;
-	}
-	if (strcmp(s, a) == 0) // 정답이면 
-		return 1;
-	else //정답이 아니면
-		return 0;
+	int result;
+	result = x + y;
+	return result;
+}
+int minus(int x, int y)
+{
+	int result;
+	result = x - y;
+	return result;
+}
+int multiply(int x, int y)
+{
+	int result;
+	result = x * y;
+	return result;
+}
+int division(int x, int y)
+{
+	int result;
+	result = x / y;
+	return result;
+}
+int remainder(int x, int y)
+{
+	int result;
+	result = x % y;
+	return result;
 }
