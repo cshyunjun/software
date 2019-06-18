068286f2b9fa4bc4ccaacee05df72feaa82664b4
hyunjun <hunjun7209@naver.com>
2019-06-18 20:58:55 +0900

    Revert "원하는 만큼 프로그램을 수헹히고 종료 할 수 있고 메뉴를 잘못 입력하면 주의문구를 출력하게 고안함"
    This reverts commit 0989b3e58364e814e8d0a81b606903e9abea34c9.

--- hw.c/hw.c/pch.cpp
+++ hw.c/hw.c/pch.cpp
@@ -12,67 +12,30 @@ double division(double x, double y);
 int remainder(double x, double y);
 void print_menu()
 {
-	printf("1. +\n2. -\n3. *\n4. /\n5. %%\n6. END\n");
+	printf("1. +\n2. -\n3. *\n4. /\n5. %%\n");
 }
 int main()
 {
 	double x, y; 
-	int sol = 0;
+	int sol;
 	print_menu();
-	
-	while (sol!=6) 
-	{   
-		printf("메뉴를 고르시오: ");
-		scanf_s("%d", &sol);
-		if (sol == 1)
-		{
-			printf("\n수를 입력하십시오.\n");
-			printf("x= ");
-			scanf_s("%lf", &x);
-			printf("y= ");
-			scanf_s("%lf", &y);
-			printf("%.2lf + %.2lf = %.2lf\n", x, y, plus(x, y));
-		}
-		else if (sol == 2)
-		{
-			printf("\n수를 입력하십시오.\n");
-			printf("x= ");
-			scanf_s("%lf", &x);
-			printf("y= ");
-			scanf_s("%lf", &y);
-			printf("%.2lf - %.2lf = %.2lf\n", x, y, minus(x, y));
-		}
-		else if (sol == 3)
-		{
-			printf("\n수를 입력하십시오.\n");
-			printf("x= ");
-			scanf_s("%lf", &x);
-			printf("y= ");
-			scanf_s("%lf", &y);
-			printf("%.2lf * %.2lf = %.2lf\n", x, y, multiply(x, y));
-		}
-		else if (sol == 4)
-		{
-			printf("\n수를 입력하십시오.\n");
-			printf("x= ");
-			scanf_s("%lf", &x);
-			printf("y= ");
-			scanf_s("%lf", &y);
-			printf("%.2lf / %.2lf = %.2lf\n", x, y, division(x, y));
-		}
-		else if (sol == 5)
-		{
-			printf("\n수를 입력하십시오.\n");
-			printf("x= ");
-			scanf_s("%lf", &x);
-			printf("y= ");
-			scanf_s("%lf", &y);
-			printf("%.0lf %% %.0lf = %d\n", x, y, remainder(x, y));
-		}
-		else if (sol < 1 || sol>6)
-			printf("올바른 menu를 고르십시요.\n");
-	}
-	printf("프로그램을 종료합니다.");
+	printf("메뉴를 고르시오: ");
+	scanf_s("%d", &sol);
+	printf("\n수를 입력하십시오.\n");
+	printf("x= ");
+	scanf_s("%lf", &x);
+	printf("y= ");
+	scanf_s("%lf", &y);
+	if (sol == 1)
+		printf("%.2lf + %.2lf = %.2lf", x, y, plus(x, y));
+	else if (sol == 2)
+		printf("%.2lf - %.2lf = %.2lf", x, y, minus(x, y));
+	else if (sol == 3)
+		printf("%.2lf * %.2lf = %.2lf", x, y, multiply(x, y));
+	else if (sol == 4)
+		printf("%.2lf / %.2lf = %.2lf", x, y, division(x, y));
+	else if (sol == 5)
+		printf("%.0lf %% %.0lf = %d", x, y, remainder(x, y));
 }
 double plus(double x, double y)
 {
